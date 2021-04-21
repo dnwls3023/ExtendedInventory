967f23e0161f8c6ff1a4ebd49094238408ace25f
dnwls3023 <68366527+dnwls3023@users.noreply.github.com>
2021-04-21 19:20:11 +0900

    Update inventory2.cpp

--- inventory2.cpp
+++ inventory2.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 
 /*
-	 ***last update 2021-04-20***
+	 ***last update 2021-04-21***
 	 인벤토리 위주로 구현
 	 기능 1 : 인벤토리를 구현한다.
 	 기능 2 : 인벤토리를 종류별로 나눈다.
-	 기능 3 : 몬스터를 구현한 뒤, 이 몬스터를 잡았을 때 아이템을 드롭시키고 
+	 기능 3 : 몬스터를 구현한 뒤, 이 몬스터를 잡았을 때 아이템을 드롭시키고
 	 아이템을 플레이어가 가져 인벤토리에 저장할 수 있도록 한다.
 	 기능 4 : 아이템을 소모성으로 제작하여 소모할 수 있게 만든다. (ex 하급 포션 : 50의 hp를 회복한다.)
 
@@ -45,14 +45,9 @@ private:
 	vector<Item> list;
 public:
 	// 아이템 한개를 추가한다.
-	void createItem(Item _item) {
-		if (check(_item))
-			list.push_back(_item);
-		else {
-			cout << "중복되는 아이템이 존재합니다." << endl;
-			cout << "아이템 이름 : " << _item.getName() << endl;
-		}
-
+	void createItem(string _name,int id,string _desc) {
+		Item tmp(_name, id, _desc);
+		list.push_back(tmp);
 	}
 	// 아이템 다수를 추가한다.
 	void createItemM(Item _item[], int size) {
@@ -73,6 +68,16 @@ public:
 		return flag;
 	}
 
+	Item findItem(int id) {
+		for (int i = 0; i < list.size(); ++i) {
+			if (id == list[i].getId()) {
+				return list[i];
+			}
+		}
+		cout << "유효하지 않은 아이디 값입니다." << endl;
+		return Item("NULL", -1, "NULL");
+	}
+
 	// 존재하는 모든 아이템을 출력한다.
 	void print() {
 		for (int i = 0; i < list.size(); ++i) {
@@ -82,14 +87,14 @@ public:
 };
 
 
-class Inventory{
+class Inventory {
 private:
 	vector<Item> list;
 public:
 	// 가지고 있는 모든 아이템을 출력한다.
 	void printAllInventory() {
 		for (int i = 0; i < list.size(); ++i) {
-			cout <<i+1<<". "<< list[i].getName() << " ";
+			cout << i + 1 << ". " << list[i].getName() << " ";
 		}
 		cout << endl;
 	}
@@ -100,18 +105,19 @@ public:
 		if (0 <= idx && idx <= list.size()) {
 			list[idx - 1].printDescription();
 		}
-			
+
 		else
 			cout << "잘못된 값을 입력했습니다." << endl;
 	}
 
 	// 아이템을 얻는다.
 	void getItem(Item _item) {
+		if (_item.getId() == -1) return;
 		list.push_back(_item);
 	}
 };
 
-class Player : public Inventory{
+class Player : public Inventory {
 private:
 	// 변수 : 가지고 있는 아이템(인벤토리), 체력, 
 	int hp;
@@ -119,14 +125,32 @@ private:
 public:
 };
 
+class Monster {
+protected:
+	int hp;
+	vector<Item> DropItems;
+public:
+	Monster(int _hp,vector<Item> _dropitem) : hp(_hp) {
+		for(int i =0;i<_dropitem.size();++i)
+			DropItems.push_back(_dropitem[i]);
+	}
+};
+
+class Zombie : public Monster {
+private:
+public:
+	Zombie(int _hp,vector<Item> _dropitem):Monster(_hp,_dropitem){}
+};
+
 int main(void) {
+	vector<Item> zombie_drop_items;
 	// 아이템 생성
-	Item potion1("하급 포션", 1, "이 포션을 마시면 50의 hp를 회복할 수 있다.");
 	ItemHandler IH;
-	IH.createItem(potion1);
+	IH.createItem("하급 포션", 1, "이 포션을 마시면 50의 hp를 회복할 수 있다.");
 	Player player;
-	player.getItem(potion1);
+	player.getItem(IH.findItem(1));
 	player.printAllInventory();
 	player.printItemInfo();
+	zombie_drop_items.push_back(IH.findItem(1));
 	return 0;
 }
